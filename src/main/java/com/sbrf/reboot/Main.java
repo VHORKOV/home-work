85e721d172a0e8a8a186d3ea126e0798d7cbe241
vhorkov <vhorkov@gmail.com>
2021-11-06 17:16:16 +0300

    class Callculator with tests

--- src/main/java/com/sbrf/reboot/Main.java
+++ src/main/java/com/sbrf/reboot/Main.java
@@ -2,5 +2,7 @@ package com.sbrf.reboot;
 
 public class Main {
     public static void main(String[] args) {
+        double res=Calculator.getDivision(1,3);
+        System.out.println(res);
     }
 }
