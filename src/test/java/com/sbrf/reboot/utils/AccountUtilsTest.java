3c16f74d3a2457a6e2a2dee4bf116432cf716263
Хорьков Вячеслав Борисович <Khorkov.V.Bo@sberbank.ru>
2021-12-05 23:22:14 +0300

    hw6,7

--- src/test/java/com/sbrf/reboot/utils/AccountUtilsTest.java
+++ src/test/java/com/sbrf/reboot/utils/AccountUtilsTest.java
@@ -1,6 +1,6 @@
 package com.sbrf.reboot.utils;
 
-import com.sbrf.reboot.dto.Account;
+import com.sbrf.reboot.service.Account;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -15,35 +15,54 @@ class AccountUtilsTest {
     @Test
     void sortedById() {
         List<Account> accounts = new ArrayList<Account>() {{
-            add(Account.builder().id(3L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
-            add(Account.builder().id(1L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
-            add(Account.builder().id(3L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
-            add(Account.builder().id(2L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+            add(Account.builder().clientId(3L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+            add(Account.builder().clientId(1L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+            add(Account.builder().clientId(3L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+            add(Account.builder().clientId(2L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
         }};
 
         AccountUtils.sortedById(accounts);
 
-        Assertions.assertEquals(1L, accounts.get(0).getId());
-        Assertions.assertEquals(2L, accounts.get(1).getId());
-        Assertions.assertEquals(3L, accounts.get(2).getId());
-        Assertions.assertEquals(3L, accounts.get(3).getId());
+        Assertions.assertEquals(1L, accounts.get(0).getClientId());
+        Assertions.assertEquals(2L, accounts.get(1).getClientId());
+        Assertions.assertEquals(3L, accounts.get(2).getClientId());
+        Assertions.assertEquals(3L, accounts.get(3).getClientId());
 
     }
 
     @Test
     void sortedByIdDate() {
         List<Account> accounts = new ArrayList<Account>() {{
-            add(Account.builder().id(1L).createDate(LocalDate.now().minusDays(4)).balance(BigDecimal.TEN).build());
-            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(3)).balance(BigDecimal.TEN).build());
-            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(1)).balance(BigDecimal.TEN).build());
-            add(Account.builder().id(2L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+            add(Account.builder().clientId(1L).createDate(LocalDate.now().minusDays(4)).balance(BigDecimal.TEN).build());
+            add(Account.builder().clientId(3L).createDate(LocalDate.now().minusDays(3)).balance(BigDecimal.TEN).build());
+            add(Account.builder().clientId(3L).createDate(LocalDate.now().minusDays(1)).balance(BigDecimal.TEN).build());
+            add(Account.builder().clientId(2L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
         }};
 
         AccountUtils.sortedByIdDate(accounts);
 
-        Assertions.assertEquals(1L, accounts.get(0).getId());
-        Assertions.assertEquals(2L, accounts.get(1).getId());
+        Assertions.assertEquals(1L, accounts.get(0).getClientId());
+        Assertions.assertEquals(2L, accounts.get(1).getClientId());
         Assertions.assertEquals(LocalDate.now().minusDays(3), accounts.get(2).getCreateDate());
         Assertions.assertEquals(LocalDate.now().minusDays(1), accounts.get(3).getCreateDate());
+    }
+
+    @Test
+    void sortedByIdDateBalance() {
+        List<Account> accounts = new ArrayList<Account>() {{
+            add(Account.builder().clientId(1L).createDate(LocalDate.now().minusDays(4)).balance(BigDecimal.ZERO).build());
+            add(Account.builder().clientId(3L).createDate(LocalDate.now().minusDays(3)).balance(BigDecimal.TEN).build());
+            add(Account.builder().clientId(3L).createDate(LocalDate.now().minusDays(1)).balance(BigDecimal.ONE).build());
+            add(Account.builder().clientId(2L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+        }};
+
+        AccountUtils.sortedByIdDateBalance(accounts);
+
+        Assertions.assertEquals(1L, accounts.get(0).getClientId());
+        Assertions.assertEquals(2L, accounts.get(1).getClientId());
+        Assertions.assertEquals(LocalDate.now().minusDays(3), accounts.get(2).getCreateDate());
+        Assertions.assertEquals(LocalDate.now().minusDays(1), accounts.get(3).getCreateDate());
+        Assertions.assertEquals(BigDecimal.ZERO, accounts.get(0).getBalance());
+        Assertions.assertEquals(BigDecimal.TEN, accounts.get(1).getBalance());
     }
 }
